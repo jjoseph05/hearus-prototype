57c4eee791ba91f2a316e3adfd37c959bfc4d6c3
Justin <justinjoseph1@gmail.com>
2014-03-23 18:28:01 -0500

    Updated search icons

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,5 +1,11 @@
 //= require_tree .
 $(document).ready(function(){
+  
+	$('#right-menu').sidr({
+      name: 'sidr-right',
+      side: 'right'
+  });
+	
 	$('#toggle').click(function(event){
 		event.preventDefault();
 	$(this).next().toggleClass('visible')
